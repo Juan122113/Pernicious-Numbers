1454e1b03014ba729ac48ae44ecee70779a34066
Juan122113 <ammiel89@hotmail.com>
2025-05-02 00:25:02 -0300

    Creating while

---
+++ main.js
@@ -0,0 +1,17 @@
+function pernicious(n){
+
+    let firstNumb = 3;
+
+    while (firstNumb <= n) {
+
+
+        firstNumb++;
+
+        // console.log(firstNumb);
+    }
+
+    console.log(firstNumb);
+
+}
+
+pernicious(25);
